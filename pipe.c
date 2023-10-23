dc0462abacdf99ce0ff37ffd4f89e70fb320a6de
Emily Liang <emilysunliang@gmail.com>
2023-10-22 23:05:55 -0700

    basic tests working

---
+++ pipe.c
@@ -0,0 +1,42 @@
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <errno.h>
+
+int main(int argc, char *argv[])
+{
+	if (argc == 0) {
+		exit(EINVAL);
+	}
+	int fd[2];
+	for (int i = 1; i < argc; i++) {
+		const char *program = argv[i];
+		if (pipe(fd) == -1)
+			perror("pipe");
+		pid_t pid = fork();
+		if (pid < 0) {
+			exit(errno);
+		}
+		if (pid == 0) {
+			// if more programs after this one:
+			if (i < argc - 1) {
+				close(fd[0]);
+				if (dup2(fd[1], STDOUT_FILENO) == -1)
+					exit(errno);
+			}
+			execlp(program, program, (char*) NULL);
+			exit(errno);
+		}
+		int st;
+		if (waitpid(pid, &st, 0) < 0) {
+			exit(errno);
+		}
+		// not first program
+		close(fd[1]);
+		if (dup2(fd[0], STDIN_FILENO) == -1)
+			exit(errno);
+	}
+	return 0;
+}
